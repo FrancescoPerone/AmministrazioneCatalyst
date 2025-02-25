d0f3ef8081c2f1b7560dfbe369f8911e34c43286
Francesco Perone <tuo_email@gmail.com>
2025-02-25 19:52:52 +0100

    Fix: Risolto problema event loop su Render

--- bot.py
+++ bot.py
@@ -72,4 +72,11 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(main())
+
